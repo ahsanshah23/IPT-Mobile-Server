304cc858a8f1fe1b2e32adf2a4cf3fa2fd532ef6
Ahsan Ali Shah <45240826+ahsanshah23@users.noreply.github.com>
2020-03-09 11:54:14 -0700

    updated

--- app.js
+++ app.js
@@ -56,6 +56,8 @@ app.use('/userlogin', (req, res) => {
         });
 });
 
+
+
 app.use('/fyp1proposal_add', (req, res) => {
     var Title = req.query.title;
     var Type = req.query.type;
@@ -69,10 +71,10 @@ app.use('/fyp1proposal_add', (req, res) => {
     var member3email = req.query.member3email;
     var supervisor_email = req.query.supervisor;
     var cosupervisor_email = req.query.cosupervisor;
-    
+    var submitted_by = req.query.submitted_by;
 
 
-    db.execute("INSERT INTO fyp1_proposal (id, project_title, project_type, area_of_interest, abstract, leader_name, leader_email, member2_name, member2_email, member3_name, member3_email, supervisor, cosupervisor, status, comment, submitted_by ) VALUES (' ', '" + Title + "' ,'" + Type + "','" + area_of_interest + "','" + abstract + "','" + leadername + "','" + leaderemail + "','" + member2name + "','" + member2email + "','" + member3name + "','" + member3email + "','" + supervisor_email + "','" + cosupervisor_email + "', '', '', '') ")
+    db.execute("INSERT INTO fyp1_proposal (id, project_title, project_type, area_of_interest, abstract, leader_name, leader_email, member2_name, member2_email, member3_name, member3_email, supervisor, cosupervisor, status, comment, submitted_by ) VALUES (' ', '" + Title + "' ,'" + Type + "','" + area_of_interest + "','" + abstract + "','" + leadername + "','" + leaderemail + "','" + member2name + "','" + member2email + "','" + member3name + "','" + member3email + "','" + supervisor_email + "','" + cosupervisor_email + "', '', '', '" + submitted_by + "') ")
         .then(results => { 
             res.send('Inserted');
         })
@@ -141,10 +143,11 @@ app.use('/fyp1proposal_list', (req, res) => {
 
 app.use('/fyp1proposal_status', (req, res) => {
     var email = req.query.Email;
+    console.log(email)
 
     db.execute("Select * from fyp_groups where leaderemail='"+email+"' OR member2email='"+email+"' OR member3email='"+email+"' ")
         .then(results => {
-            // console.log(results[0].length);
+             console.log(results[0].length);
             if(results[0].length>0)
             {
                 res.send("1");
@@ -198,9 +201,10 @@ app.use('/fyp1midevaluation_add', (req, res) => {
     var comments = req.query.comments;
     var evaluator = req.query.evaluator;
     var coevaluator = req.query.coevaluator;
+    var submitted_by = req.query.submitted_by;
 
 
-    db.execute(" INSERT INTO fyp1_mid_evaluation (ID, Project_Title, leader_email, leader_name, member2_email, member2_name, member3_email, member3_name, supervisor_email, cosupervisor_email, Project_Status, Criteria1_Marks, Criteria2_Marks, Criteria3_Marks, Criteria4_Marks, Criteria5_Marks, Criteria6_Marks, Deliverable1, Deliverable2, Deliverable3, Deliverable4, Deliverable5, Recommended_Changes, Comments_To_FYP_Committee, Evaluator1_Name, Evaluator2_Name ) VALUES (' ', '" + Title + "', '" + leadername + "', '" + leaderemail + "','" + member2name + "','" + member2email + "','" + member3name + "','" + member3email + "','" + supervisor_email + "','" + cosupervisor_email + "', '"+status+"', '"+markcriteria1+"','"+markcriteria2+"','"+markcriteria3+"','"+markcriteria4+"','"+markcriteria5+"','"+markcriteria6+"','"+deliverable1+"','"+deliverable2+"','"+deliverable3+"','"+deliverable4+"','"+deliverable5+"', '"+changes+"','"+comments+"','"+evaluator+"','"+coevaluator+"') ")
+    db.execute(" INSERT INTO fyp1_mid_evaluation (ID, Project_Title, leader_email, leader_name, member2_email, member2_name, member3_email, member3_name, supervisor_email, cosupervisor_email, Project_Status, Criteria1_Marks, Criteria2_Marks, Criteria3_Marks, Criteria4_Marks, Criteria5_Marks, Criteria6_Marks, Deliverable1, Deliverable2, Deliverable3, Deliverable4, Deliverable5, Recommended_Changes, Comments_To_FYP_Committee, Evaluator1_Name, Evaluator2_Name, submitted_by ) VALUES (' ', '" + Title + "', '" + leadername + "', '" + leaderemail + "','" + member2name + "','" + member2email + "','" + member3name + "','" + member3email + "','" + supervisor_email + "','" + cosupervisor_email + "', '"+status+"', '"+markcriteria1+"','"+markcriteria2+"','"+markcriteria3+"','"+markcriteria4+"','"+markcriteria5+"','"+markcriteria6+"','"+deliverable1+"','"+deliverable2+"','"+deliverable3+"','"+deliverable4+"','"+deliverable5+"', '"+changes+"','"+comments+"','"+evaluator+"','"+coevaluator+"','"+submitted_by+"') ")
         .then(results => { 
             res.send('Inserted');
         })                                                      
@@ -257,9 +261,10 @@ app.use('/fyp1finalevaluation_add', (req, res) => {
 
     var Jury1_Name = req.query.evaluator;
     var Jury2_Name = req.query.coevaluator;
+    var submitted_by = req.query.submitted_by;
 
 
-    db.execute(" INSERT INTO fyp1_final_evaluation (ID, Project_Title, leader_name, member2_name, member3_name, leader_email, member2_email, member3_email, supervisor_email, cosupervisor_email, Deliverable1_Name, Deliverable2_Name, Deliverable3_Name, Deliverable4_Name, Deliverable5_Name, Deliverable1_Completed, Deliverable2_Completed, Deliverable3_Completed, Deliverable4_Completed, Deliverable5_Completed, FYP2_Deliverables, Member1_Marks, Member2_Marks, Member3_Marks, Jury1_Name, Jury2_Name ) VALUES (' ', '" + Title + "', '" + leadername + "', '" + member2name + "','" + member3name + "','" + leaderemail + "','" + member2email + "','" + member3email + "','" + supervisor_email + "','" + cosupervisor_email + "', '"+Deliverable1_Name+"', '"+Deliverable2_Name+"','"+Deliverable3_Name+"','"+Deliverable4_Name+"','"+Deliverable5_Name+"','"+Deliverable1_Completed+"','"+Deliverable2_Completed+"','"+Deliverable3_Completed+"','"+Deliverable4_Completed+"','"+Deliverable5_Completed+"','"+FYP2_Deliverables+"','"+Member1_Marks+"', '"+Member2_Marks+"','"+Member3_Marks+"','"+Jury1_Name+"','"+Jury2_Name+"') ")
+    db.execute(" INSERT INTO fyp1_final_evaluation (ID, Project_Title, leader_name, member2_name, member3_name, leader_email, member2_email, member3_email, supervisor_email, cosupervisor_email, Deliverable1_Name, Deliverable2_Name, Deliverable3_Name, Deliverable4_Name, Deliverable5_Name, Deliverable1_Completed, Deliverable2_Completed, Deliverable3_Completed, Deliverable4_Completed, Deliverable5_Completed, FYP2_Deliverables, Member1_Marks, Member2_Marks, Member3_Marks, Jury1_Name, Jury2_Name, submitted_by ) VALUES (' ', '" + Title + "', '" + leadername + "', '" + member2name + "','" + member3name + "','" + leaderemail + "','" + member2email + "','" + member3email + "','" + supervisor_email + "','" + cosupervisor_email + "', '"+Deliverable1_Name+"', '"+Deliverable2_Name+"','"+Deliverable3_Name+"','"+Deliverable4_Name+"','"+Deliverable5_Name+"','"+Deliverable1_Completed+"','"+Deliverable2_Completed+"','"+Deliverable3_Completed+"','"+Deliverable4_Completed+"','"+Deliverable5_Completed+"','"+FYP2_Deliverables+"','"+Member1_Marks+"', '"+Member2_Marks+"','"+Member3_Marks+"','"+Jury1_Name+"','"+Jury2_Name+"','"+submitted_by+"') ")
         .then(results => { 
             res.send('Inserted');
         })                                                      
@@ -355,7 +360,9 @@ app.use('/fyp2finalevaluationexternal_add', (req, res) => {
     var Jury1_Name = req.query.evaluator;
     var Jury2_Name = req.query.coevaluator;
 
-    db.execute("INSERT INTO fyp2_final_evaluation_external (ID, Project_Title, leader_name, member2_name, member3_name, supervisor_email, cosupervisor_email, leader_email, member2_email, member3_email, leader_Marks, Member2_Marks, Member3_Marks, Jury1_Name, Jury2_Name ) VALUES (' ', '" + Project_Title + "' ,'" + leader_name + "','" + member2_name + "','" + member3_name + "','" + supervisor_email + "','" + cosupervisor_email + "','" + leader_email + "','" + member2_email + "','" + member3_email + "','" + leader_Marks + "','" + Member2_Marks + "','" + Member3_Marks + "','" + Jury1_Name + "','" + Jury2_Name + "') ")
+    var submitted_by = req.query.submitted_by;
+
+    db.execute("INSERT INTO fyp2_final_evaluation_external (ID, Project_Title, leader_name, member2_name, member3_name, supervisor_email, cosupervisor_email, leader_email, member2_email, member3_email, leader_Marks, Member2_Marks, Member3_Marks, Jury1_Name, Jury2_Name, submitted_by ) VALUES (' ', '" + Project_Title + "' ,'" + leader_name + "','" + member2_name + "','" + member3_name + "','" + supervisor_email + "','" + cosupervisor_email + "','" + leader_email + "','" + member2_email + "','" + member3_email + "','" + leader_Marks + "','" + Member2_Marks + "','" + Member3_Marks + "','" + Jury1_Name + "','" + Jury2_Name + "','" + submitted_by + "') ")
     .then(results => {
         res.send('Inserted');
     })
